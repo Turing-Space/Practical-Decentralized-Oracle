69a60d8ad3247f902783febc67c8070109593c03
Ting Ting Lee <tingtinglee@Tings-MacBook-Pro.local>
2018-09-12 13:30:18 +0800

    remove comments

--- contracts/Client.sol
+++ contracts/Client.sol
@@ -15,28 +15,14 @@ contract Client {
     function vote(bool _value) public {
         // sync before voting to prevent voting to past camp
         syncToNewlyOpenedSeq();
-
-        // bool voteCampFinished = C.acceptVote(seq, _value);
-
         C.acceptVote(seq, _value);
-        
-        // if(voteCampFinished) {
-        //     // TODO: client get their deposit and some bonus(maybe from others who lost their deposit) back if his vote in this seq is the final state
-        //     // otherwise, the deposit is lost 
-        //     storeFinalState();
-        // }
     }
-    
-    // function storeFinalState() public {
-    //     finalResultOnCamp = C.finalResultOnCamp(seq);
-    // }
 
     function queryFinalState() public view returns (uint8) {
         return C.finalResultOnCamp(C.last_finalized_seq());
     }
 
     function syncToNewlyOpenedSeq() private {
-        // TODO: register client to custodian whenever client request to sync?
         seq = C.newly_opened_seq();
     }
 }
